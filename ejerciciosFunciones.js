7ed1223aee8e605f35b57f43d1394fce1306a545
leydy <leydymayumy@gmail.com>
2020-03-13 21:44:55 -0500

    x

--- ejerciciosFunciones.js
+++ ejerciciosFunciones.js
@@ -1,13 +1,13 @@
 function celciusFahrenheit(celsius){
   var cTemp = celsius;
-  var cToFahr = cTemp * 9 / 5 + 32;
+  var cToFahr = cTemp * 9/5 + 32;
   var convert = cTemp+'\xB0C son ' +cToFahr+ ' \xB0F';
     console.log(convert);
 }
 
 function fahrenheittoCelcius(fahrenheit) {
   var fTemp = fahrenheit;
-  var fToCel = (fTemp - 32) * 5 / 9;
+  var fToCel = (fTemp - 32) * 5/9;
   var convert = fTemp+'\xB0F son ' +fToCel+ ' \xB0C';
     console.log(convert);
 } 
