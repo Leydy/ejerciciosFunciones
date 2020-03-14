09d0833cc5c610520e9507f51785ae7349e0c16e
Leydy <leydymayumy@gmail.com>
2020-03-13 21:17:01 -0500

    convertidor de grados xd

--- ejerciciosFunciones.js
+++ ejerciciosFunciones.js
@@ -1,14 +1,14 @@
 function celciusFahrenheit(celsius){
   var cTemp = celsius;
   var cToFahr = cTemp * 9 / 5 + 32;
-  var convert = cTemp+'\xB0C son ' + cToFahr + ' \xB0F';
+  var convert = cTemp+'\xB0C son ' +cToFahr+ ' \xB0F';
     console.log(convert);
 }
 
 function fahrenheittoCelcius(fahrenheit) {
   var fTemp = fahrenheit;
   var fToCel = (fTemp - 32) * 5 / 9;
-  var convert = fTemp+'\xB0F son ' + fToCel + ' \xB0C';
+  var convert = fTemp+'\xB0F son ' +fToCel+ ' \xB0C';
     console.log(convert);
 } 
 celciusFahrenheit(60);
